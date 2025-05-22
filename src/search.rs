242bb22fb90a2fbd5dde174ecbbe36ebcc160b92
lbulhoes <dev@lucasbulhoes.com>
2025-05-22 06:28:12 -0300

    fixing binary_search and adding lower_bound and upper_bound

--- src/search.rs
+++ src/search.rs
@@ -1,3 +1,8 @@
+use std::cmp::Ordering;
+
+#[cfg(test)]
+mod tests;
+
 pub fn binary_search<T: Ord>(arr: &[T], target: T) -> Option<usize> {
     let mut left = 0;
     let mut right = arr.len();
@@ -5,11 +10,49 @@ pub fn binary_search<T: Ord>(arr: &[T], target: T) -> Option<usize> {
     while left < right {
         let mid = left + ((right - left) / 2);
         match target.cmp(&arr[mid]) {
-            std::cmp::Ordering::Less => right = left,
-            std::cmp::Ordering::Greater => left += 1,
-            std::cmp::Ordering::Equal => return Some(left),
-            
+            Ordering::Equal => return Some(mid),
+            Ordering::Greater => left = mid + 1,
+            Ordering::Less => right = mid,
         }
     }
     None
 }
+
+pub fn lower_bound<T: Ord>(arr: &[T], target: T) -> Option<usize> {
+    let mut left = 0;
+    let mut right = arr.len();
+
+    while left < right {
+        let mid = left + ((right - left) / 2);
+        match target.cmp(&arr[mid]) {
+            Ordering::Less | Ordering::Equal => right = mid,
+            Ordering::Greater => left = mid + 1,
+        }
+    }
+    
+    if left < arr.len() {
+        Some(left)
+    } else {
+        None
+    }
+}
+
+pub fn upper_bound<T: Ord>(arr: &[T], target: T) -> Option<usize> {
+    let mut left = 0;
+    let mut right = arr.len();
+
+    while left < right {
+        let mid = left + ((right - left) / 2);
+        match target.cmp(&arr[mid]) {
+            Ordering::Less => right = mid,
+            Ordering::Greater | Ordering::Equal => left = mid + 1,
+        }
+    }
+    
+    if left < arr.len() {
+        Some(left)
+    } else {
+        None
+    }
+}
+
